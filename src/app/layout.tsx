423ce3eb5efdc495f8a104de594c4afe28566a5e
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-05-31 01:36:04 -0300

    adiciona viewport ao head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="pt-br">
-      <head></head>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={nunito.className}>
         <StyledComponentsRegistry>
           <Header />
